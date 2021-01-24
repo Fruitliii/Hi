641aaad3040b1afefee382982ec56054b8cdba37
Tony Li <xiao-benben@hotmail.com>
2021-01-23 19:38:06 -0500

    first time

---
+++ py01.py
@@ -0,0 +1,9 @@
+celsius = input('What is the tempeture in celsius now?')
+celsius = int(celsius)
+print(int(celsius))
+print(type(celsius))
+if type(celsius) == int:
+	fahrenheit = 0
+	fahrenheit = float(fahrenheit)
+	fahrenheit = float(celsius)*(9/5) + 32
+	print('In fahrenheit is: ', fahrenheit)
